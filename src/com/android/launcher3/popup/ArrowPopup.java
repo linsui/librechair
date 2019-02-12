af4addb9431c512caa32468178c617e5d47d7aa8
Jon Miranda <jonmiranda@google.com>
2019-02-13 03:03:06 +0800

    Ensure ArrowPopup alpha is set to 1 at the end of the opening animation.
    The fade in animation doesn't change the alpha until the reveal animation
    has started — so when animators are turned off the alpha never gets updated.
    
    Bug: 124252507
    Change-Id: I19bb77e16389ff870e05692b1aea8483033485ac
    Signed-off-by: oldosfan <luangruo@yahoo.com>

--- src/com/android/launcher3/popup/ArrowPopup.java
+++ src/com/android/launcher3/popup/ArrowPopup.java
@@ -379,6 +379,7 @@ public abstract class ArrowPopup extends AbstractFloatingView {
         openAnim.addListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
+                setAlpha(1f);
                 announceAccessibilityChanges();
                 mOpenCloseAnimator = null;
             }
