4c83f64094d562622006f8cf023859b6d9ca3629
oldosfan <luangruo42@gmail.com>
2019-06-30 09:00:44 +0800

    globalsearch: create metasearch provider powered by searx
    This provider is WIP, and will eventually be cherry-picked upstream

---
+++ lawnchair/src/ch/deletescape/lawnchair/globalsearch/activity/WebSearchProviderActivity.java
@@ -0,0 +1,33 @@
+/*
+ *     Copyright (c) 2017-2019 the Lawnchair team
+ *     Copyright (c)  2019 oldosfan (would)
+ *     This file is part of Lawnchair Launcher.
+ *
+ *     Lawnchair Launcher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Lawnchair Launcher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Lawnchair Launcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.deletescape.lawnchair.globalsearch.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.android.launcher3.R;
+
+public class WebSearchProviderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_search_provider);
+    }
+}
---
+++ lawnchair/src/ch/deletescape/lawnchair/globalsearch/providers/BuiltInSearchProvider.kt
@@ -0,0 +1,48 @@
+/*
+ *     Copyright (c) 2017-2019 the Lawnchair team
+ *     Copyright (c)  2019 oldosfan (would)
+ *     This file is part of Lawnchair Launcher.
+ *
+ *     Lawnchair Launcher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Lawnchair Launcher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Lawnchair Launcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.deletescape.lawnchair.globalsearch.providers
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import ch.deletescape.lawnchair.globalsearch.SearchProvider
+import ch.deletescape.lawnchair.globalsearch.activity.WebSearchProviderActivity
+import com.android.launcher3.R
+import kotlinx.android.synthetic.lawnchair.activity_web_search_provider.view.*
+
+class BuiltInSearchProvider(c: Context) : SearchProvider(c) {
+    override val name: String
+        get() = context.getString(R.string.search_provider_built_in) + " [WIP]"
+    override val supportsVoiceSearch: Boolean
+        get() = true
+    override val supportsAssistant: Boolean
+        get() = false
+    override val supportsFeed: Boolean
+        get() = false
+
+    override fun startSearch(callback: (intent: Intent) -> Unit) {
+        val i = Intent(context, WebSearchProviderActivity::class.java);
+        callback.invoke(i);
+    }
+
+    override fun getIcon(): Drawable {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
