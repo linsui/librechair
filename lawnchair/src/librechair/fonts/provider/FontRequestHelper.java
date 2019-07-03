294522a070e812e9f8d78cc47d516b2c958a549f
oldosfan <luangruo42@gmail.com>
2019-07-03 09:00:01 +0800

    fonts: add custom Google Fonts provider based on the Google Fonts API

--- lawnchair/src/librechair/fonts/provider/FontRequestHelper.java
+++ lawnchair/src/librechair/fonts/provider/FontRequestHelper.java
@@ -19,21 +19,78 @@
 
 package librechair.fonts.provider;
 
+import static android.support.v4.provider.FontsContractCompat.FontRequestCallback.FAIL_REASON_FONT_LOAD_ERROR;
+import static android.support.v4.provider.FontsContractCompat.FontRequestCallback.FAIL_REASON_FONT_NOT_FOUND;
+import static android.support.v4.provider.FontsContractCompat.FontRequestCallback.FAIL_REASON_MALFORMED_QUERY;
+
 import android.content.Context;
+import android.graphics.Typeface;
 import android.os.Handler;
 import android.support.v4.provider.FontRequest;
 import android.support.v4.provider.FontsContractCompat.FontRequestCallback;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.HashMap;
+import java.util.Map;
+import org.apache.commons.io.IOUtils;
 
 public class FontRequestHelper {
-    public static void postFontRequest(Context context, FontRequest request, FontRequestCallback callback, Handler handler) {
-        /*
-         * TODO: Liberate fonts
-         */
+
+    public static final String GOOGLE_FONT_KEY = "AIzaSyD-tjZZSSDbq0ggoJMydogsGlpzcBTaxTw";
+
+    public static void postFontRequest(Context context, FontRequest request,
+            FontRequestCallback callback, Handler handler) {
+        try {
+            String[] split = request.getQuery().split("[&?]");
+            HashMap<String, String> processedRequest = new HashMap<>();
+            for (String iter : split) {
+                processedRequest.put(iter.split("=")[0], iter.split("=")[1]);
+            }
+            postFontRequest(context, processedRequest, callback, handler);
+        } catch (IndexOutOfBoundsException e) {
+            callback.onTypefaceRequestFailed(FAIL_REASON_MALFORMED_QUERY);
+        }
     }
-    public static void postFontRequest(Context context, HashMap<String, String> request, FontRequestCallback callback, Handler handler) {
-        /*
-         * TODO: Liberate fonts
-         */
+
+    public static void postFontRequest(Context context, Map<String, String> request,
+            FontRequestCallback callback, Handler handler) {
+
+        handler.post(() -> {
+            String apiCall = String.format("https://www.googleapis.com/webfonts/v1/webfonts?key=%s", GOOGLE_FONT_KEY);
+            File cachedTypeface = new File(context.getCacheDir(), "typeface_google_" + request.hashCode() + "_cached.ttf");
+            if (cachedTypeface.exists()) {
+                callback.onTypefaceRetrieved(Typeface.createFromFile(cachedTypeface));
+                return;
+            }
+            try {
+                String rawJson = IOUtils.toString(new URL(apiCall).openConnection().getInputStream(),
+                        Charset.defaultCharset());
+                JsonParser jsonParser = new JsonParser();
+                JsonObject jsonObject = jsonParser.parse(rawJson).getAsJsonObject();
+                JsonArray fontItems = jsonObject.getAsJsonArray("items");
+                for (JsonElement object : fontItems) {
+                    JsonArray variants = object.getAsJsonObject().getAsJsonArray("variants");
+                    JsonObject files = object.getAsJsonObject().getAsJsonObject("files");
+                    if (variants.contains(new JsonPrimitive(request.get("weight")))) {
+                        IOUtils.copy(new URL(files.getAsJsonPrimitive(request.get("weight")).toString()).openConnection().getInputStream(), new FileOutputStream(cachedTypeface));
+                        callback.onTypefaceRetrieved(Typeface.createFromFile(cachedTypeface));
+                        return;
+                    }
+                }
+                callback.onTypefaceRequestFailed(FAIL_REASON_FONT_NOT_FOUND);
+
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+                callback.onTypefaceRequestFailed(FAIL_REASON_FONT_LOAD_ERROR);
+            }
+        });
     }
 }
