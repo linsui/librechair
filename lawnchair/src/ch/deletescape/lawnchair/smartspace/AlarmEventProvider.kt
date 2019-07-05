d53514b5d4d179b479a1a3ad1073b00ae3b27033
oldosfan <luangruo42@gmail.com>
2019-07-05 14:46:48 +0800

    smartspace: migrate upcoming alarm provider to the handler-thread approach

--- lawnchair/src/ch/deletescape/lawnchair/smartspace/AlarmEventProvider.kt
+++ lawnchair/src/ch/deletescape/lawnchair/smartspace/AlarmEventProvider.kt
@@ -39,11 +39,12 @@ package ch.deletescape.lawnchair.smartspace
 import android.app.AlarmManager
 import android.content.Context
 import android.os.Handler
+import android.os.HandlerThread
+import android.os.SystemClock
 import android.support.annotation.Keep
 import android.util.Log
 import ch.deletescape.lawnchair.drawableToBitmap
 import ch.deletescape.lawnchair.formatTime
-import ch.deletescape.lawnchair.util.Temperature
 import com.android.launcher3.R
 import java.util.*
 import java.util.concurrent.TimeUnit
@@ -53,14 +54,8 @@ import kotlin.collections.ArrayList
 @Keep class AlarmEventProvider(controller: LawnchairSmartspaceController) :
         LawnchairSmartspaceController.DataProvider(controller) {
 
-    private var silentlyFail: Boolean = false
-    private val iconProvider = WeatherIconProvider(controller.context)
-    private val weather = LawnchairSmartspaceController
-            .WeatherData(iconProvider.getIcon("-1"), Temperature(0, Temperature.Unit.Celsius), "")
-    private var card: LawnchairSmartspaceController.CardData? = null
-    private val contentResolver = controller.context.contentResolver
-    private var refreshThread: Thread? = null;
-    private val handler: Handler = Handler()
+    private val handlerThread by lazy { HandlerThread("") }
+    private val handler by lazy { Handler(handlerThread.looper) }
 
     init {
         Log.d(javaClass.name, "class initializer: init")
@@ -68,26 +63,6 @@ import kotlin.collections.ArrayList
     }
 
     private fun updateInformation() {
-        if (refreshThread == null) {
-            refreshThread = Thread {
-                run {
-                    while (true) {
-                        try {
-                            Thread.sleep(10000)
-                        } catch (e: InterruptedException) {
-                            break
-                        }
-                        if (refreshThread!!.isInterrupted) {
-                            break;
-                        }
-                        handler.post {
-                            updateInformation()
-                        }
-                    }
-                }
-            }
-            refreshThread!!.start();
-        }
         val alarmManager =
                 controller.context.getSystemService(Context.ALARM_SERVICE) as AlarmManager;
         if (alarmManager.nextAlarmClock != null && alarmManager.nextAlarmClock!!.triggerTime - System.currentTimeMillis() <= TimeUnit.MINUTES.toMillis(
@@ -99,21 +74,20 @@ import kotlin.collections.ArrayList
                 controller.context.getString(R.string.resuable_text_alarm)));
             string.add(LawnchairSmartspaceController.Line(
                 formatTime(Date(alarmClock.triggerTime), controller.context)))
-            updateData(weather, LawnchairSmartspaceController.CardData(
+            updateData(null, LawnchairSmartspaceController.CardData(
                 drawableToBitmap(controller.context.getDrawable(R.drawable.ic_alarm_on_black_24dp)),
                 string, true))
         } else {
-            updateData(weather, card = null)
+            updateData(null, null)
         }
     }
 
     override fun onDestroy() {
-        if (refreshThread != null) {
-            refreshThread!!.interrupt()
-        }
+        handlerThread.quitSafely();
     }
 
     override fun forceUpdate() {
         updateInformation()
+        handler.postAtTime(this::forceUpdate, SystemClock.uptimeMillis())
     }
 }
