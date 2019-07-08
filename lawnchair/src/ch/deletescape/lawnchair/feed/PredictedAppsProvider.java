cdfeed6774dee4062ff2f91ca6d50318e63d687f
oldosfan <luangruo42@gmail.com>
2019-07-08 12:43:31 +0800

    feed: add preliminary implementation of predicted apps provider

---
+++ lawnchair/src/ch/deletescape/lawnchair/feed/PredictedAppsProvider.java
@@ -0,0 +1,58 @@
+/*
+ *     Copyright (c) 2017-2019 the Lawnchair team
+ *     Copyright (c)  2019 oldosfan (would)
+ *     This file is part of Lawnchair Launcher.
+ *
+ *     Lawnchair Launcher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Lawnchair Launcher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Lawnchair Launcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.deletescape.lawnchair.feed;
+
+import android.content.Context;
+import com.google.android.apps.nexuslauncher.allapps.PredictionRowView;
+import java.util.List;
+
+public class PredictedAppsProvider extends FeedProvider {
+
+    private PredictionRowView rowView;
+
+    public PredictedAppsProvider(Context c) {
+        super(c);
+    }
+
+    @Override
+    public void onFeedShown() {
+
+    }
+
+    @Override
+    public void onFeedHidden() {
+
+    }
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+
+    @Override
+    public List<Card> getCards() {
+        return null;
+    }
+}
