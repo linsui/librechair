5506766eb85c727b3f6ae13d44b9884f1e7873d2
oldosfan <luangruo42@gmail.com>
2019-07-12 20:40:35 +0800

    feed: add basic sorting algorithm code

---
+++ lawnchair/src/ch/deletescape/lawnchair/feed/OrderedSortingAlgorithm.java
@@ -0,0 +1,35 @@
+/*
+ *     Copyright (c) 2017-2019 the Lawnchair team
+ *     Copyright (c)  2019 oldosfan (would)
+ *     This file is part of Lawnchair Launcher.
+ *
+ *     Lawnchair Launcher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Lawnchair Launcher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Lawnchair Launcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.deletescape.lawnchair.feed;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.jetbrains.annotations.NotNull;
+
+public class OrderedSortingAlgorithm implements SortingAlgorithm<Card> {
+
+    @NotNull
+    @Override
+    public List<Card> sort(@NotNull List<? extends Card> cards) {
+        List<Card> cards1 = new ArrayList<>();
+        cards1.addAll(cards);
+        return cards1;
+    }
+}
