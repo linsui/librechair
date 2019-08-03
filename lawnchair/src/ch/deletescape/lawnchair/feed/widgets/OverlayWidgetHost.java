c2e7c19bd3aa7d64856a46dffc9b169c74d84434
oldosfan <luangruo42@gmail.com>
2019-08-03 09:52:35 +0800

    feed: disallow touch event interception only if widget is scrollable in the widget list adapter

--- lawnchair/src/ch/deletescape/lawnchair/feed/widgets/OverlayWidgetHost.java
+++ lawnchair/src/ch/deletescape/lawnchair/feed/widgets/OverlayWidgetHost.java
@@ -26,6 +26,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.RemoteViews;
 import com.android.launcher3.R;
 
@@ -39,6 +41,30 @@ public class OverlayWidgetHost extends AppWidgetHost {
     protected AppWidgetHostView onCreateView(Context context, int appWidgetId,
             AppWidgetProviderInfo appWidget) {
         return new AppWidgetHostView(context) {
+
+            private boolean scrollable = false;
+
+            private boolean checkScrollableRecursively(ViewGroup viewGroup) {
+                if (viewGroup instanceof AdapterView) {
+                    return true;
+                } else {
+                    for (int i = 0; i < viewGroup.getChildCount(); i++) {
+                        View child = viewGroup.getChildAt(i);
+                        if (child instanceof ViewGroup) {
+                            if (checkScrollableRecursively((ViewGroup) child)) {
+                                return true;
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
+
+            @Override
+            protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+                super.onLayout(changed, left, top, right, bottom);
+                scrollable = checkScrollableRecursively(this);
+            }
 
             @Override
             public void setAppWidget(int appWidgetId, AppWidgetProviderInfo info) {
@@ -59,7 +85,7 @@ public class OverlayWidgetHost extends AppWidgetHost {
 
             @Override
             public boolean onInterceptTouchEvent(MotionEvent event) {
-                if (getParent() != null && getParent().getParent() != null) {
+                if (scrollable && event.getAction() == MotionEvent.ACTION_DOWN) {
                     getParent().requestDisallowInterceptTouchEvent(true);
                     getParent().getParent().requestDisallowInterceptTouchEvent(true);
                 }
