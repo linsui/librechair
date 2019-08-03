e9ea7f5f4a6c5b11eff6d67fcee72728953714aa
oldosfan <luangruo42@gmail.com>
2019-08-03 19:01:19 +0800

    feed: add calendar to the daily summary feed provider

--- lawnchair/src/ch/deletescape/lawnchair/feed/DailySummaryFeedProvider.java
+++ lawnchair/src/ch/deletescape/lawnchair/feed/DailySummaryFeedProvider.java
@@ -20,9 +20,11 @@
 package ch.deletescape.lawnchair.feed;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.drawable.Drawable;
+import android.provider.CalendarContract;
 import android.support.annotation.NonNull;
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
@@ -35,7 +37,9 @@ import com.android.launcher3.R;
 import com.google.android.apps.nexuslauncher.graphics.IcuDateTextView;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 public class DailySummaryFeedProvider extends FeedProvider {
 
@@ -76,7 +80,10 @@ public class DailySummaryFeedProvider extends FeedProvider {
                     RecyclerView recyclerView = v.findViewById(R.id.daily_summary_information);
                     Adapter adapter = new Adapter(parent.getContext());
                     recyclerView.setAdapter(adapter);
-                    recyclerView.setLayoutManager(new LinearLayoutManager(parent.getContext()));
+                    GridLayoutManager layoutManager;
+                    recyclerView.setLayoutManager(
+                            layoutManager = new GridLayoutManager(parent.getContext(),
+                                    1)); /* TODO proper grid layout span sizes */
                     adapter.refresh();
                     return v;
                 },
@@ -94,12 +101,39 @@ class Adapter extends RecyclerView.Adapter {
         this.context = context;
     }
 
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
     public void refresh() {
         items.clear();
-        items.add(new DailySummaryItem(LawnchairUtilsKt
-                .duplicateAndSetColour(context.getDrawable(R.drawable.ic_assessment_black_24dp),
-                        LawnchairUtilsKt.getColorAttrib(context.getTheme(), R.attr.colorAccent)),
-                "Debug"));
+        Date currentTime = new Date();
+        String query =
+                "(( " + CalendarContract.Events.DTSTART + " >= " + currentTime.getTime()
+                        + " ) AND ( " + CalendarContract.Events.DTSTART + " <= " + LawnchairUtilsKt
+                        .tomorrow(currentTime).getTime() + " ))";
+        Cursor calendarEvents = context.getContentResolver()
+                .query(CalendarContract.Events.CONTENT_URI,
+                        new String[]{CalendarContract.Instances.TITLE,
+                                CalendarContract.Instances.DTSTART,
+                                CalendarContract.Instances.DTEND,
+                                CalendarContract.Instances.DESCRIPTION,
+                                CalendarContract.Events._ID,
+                                CalendarContract.Instances.CUSTOM_APP_PACKAGE,
+                                CalendarContract.Events.EVENT_LOCATION}, query, null,
+                        CalendarContract.Instances.DTSTART + " ASC");
+        if (calendarEvents.getCount() > 0) {
+            items.add(new DailySummaryItem(LawnchairUtilsKt
+                    .duplicateAndSetColour(
+                            Objects.requireNonNull(
+                                    context.getDrawable(R.drawable.ic_event_black_24dp)),
+                            LawnchairUtilsKt
+                                    .getColorAttrib(context.getTheme(), R.attr.colorAccent)),
+                    context.getResources()
+                            .getQuantityString(R.plurals.title_daily_briefing_calendar_events,
+                                    calendarEvents.getCount(), calendarEvents.getCount())));
+        }
         notifyDataSetChanged();
     }
 
