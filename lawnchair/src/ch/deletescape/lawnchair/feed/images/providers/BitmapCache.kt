8b2616b6e4ba6dbbba912fbdf5980fa7c9ec60f4
oldosfan <luangruo@yahoo.com>
2019-08-31 20:30:33 +0800

    images: add stub implementation of the image provider system

---
+++ lawnchair/src/ch/deletescape/lawnchair/feed/images/providers/BitmapCache.kt
@@ -0,0 +1,35 @@
+/*
+ *     Copyright (c) 2017-2019 the Lawnchair team
+ *     Copyright (c)  2019 oldosfan (would)
+ *     This file is part of Lawnchair Launcher.
+ *
+ *     Lawnchair Launcher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Lawnchair Launcher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Lawnchair Launcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.deletescape.lawnchair.feed.images.providers
+
+import android.content.Context
+import android.graphics.Bitmap
+
+object BitmapCache {
+    private val cache: MutableMap<String, Pair<Long, Bitmap>> = mutableMapOf()
+
+    fun getBitmap(provider: ImageProvider, c: Context): Bitmap {
+        if (System.currentTimeMillis() - (cache[provider::class.qualifiedName]?.first ?: 0 ) >= provider.expiryTime) {
+            return provider.getBitmap(c).also { cache[provider::class.qualifiedName!!] = System.currentTimeMillis() to it }
+        } else {
+            return cache[provider::class.qualifiedName]!!.second
+        }
+    }
+}
