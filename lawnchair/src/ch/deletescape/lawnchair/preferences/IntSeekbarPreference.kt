b8091573ffcb88888128a8dc713c4e5af9c424cd
oldosfan <luangruo@yahoo.com>
2019-09-27 11:47:22 +0800

    feed: hide toolbar in the note management screen prefs: improve integer seekbar preference
    Signed-off-by: oldosfan <luangruo@yahoo.com>

--- lawnchair/src/ch/deletescape/lawnchair/preferences/IntSeekbarPreference.kt
+++ lawnchair/src/ch/deletescape/lawnchair/preferences/IntSeekbarPreference.kt
@@ -37,132 +37,20 @@
 package ch.deletescape.lawnchair.preferences
 
 import android.content.Context
-import android.content.res.ColorStateList
-import android.os.Handler
-import android.os.HandlerThread
 import android.util.AttributeSet
-import android.view.ContextMenu
-import android.view.MenuItem
-import android.view.View
-import android.widget.SeekBar
-import android.widget.TextView
-import androidx.preference.Preference
-import androidx.preference.PreferenceViewHolder
-import ch.deletescape.lawnchair.colors.ColorEngine
-import com.android.launcher3.R
+import kotlin.math.roundToInt
 
 open class IntSeekbarPreference @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) :
-        Preference(context, attrs, defStyleAttr), SeekBar.OnSeekBarChangeListener, View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener, ColorEngine.OnColorChangeListener {
-
-    private var mSeekbar: SeekBar? = null
-    protected var mValueText: TextView? = null
-    protected var min: Int = 0
-    protected var max: Int = 0
-    protected var current: Int = 0
-    protected var defaultValue: Int = 0
-    private var multiplier: Int = 0
-    private var format: String? = null
-    private var lastPersist: Int? = null
-
-    private val handlerThread = HandlerThread("debounce").apply { start() }
-    private val persistHandler = Handler(handlerThread.looper)
-
-    open val allowResetToDefault = true
-
-    init {
-        layoutResource = R.layout.preference_seekbar
-        init(context, attrs!!)
+        SeekbarPreference(context, attrs, defStyleAttr) {
+    override fun updateSummary() {
+        mValueText!!.text = String.format(format!!, current.roundToInt() * multiplier)
     }
 
-    private fun init(context: Context, attrs: AttributeSet) {
-        val ta = context.obtainStyledAttributes(attrs, R.styleable.SeekbarPreference)
-        min = ta.getInt(R.styleable.IntSeekbarPreference_minValueI, 0)
-        max = ta.getInt(R.styleable.IntSeekbarPreference_maxValueI, 100)
-        multiplier = ta.getInt(R.styleable.SeekbarPreference_summaryMultiplier, 1)
-        format = ta.getString(R.styleable.SeekbarPreference_summaryFormat)
-        defaultValue = ta.getInt(R.styleable.IntSeekbarPreference_defaultSeekbarValueI, min)
-        if (format == null) {
-            format = "%.2d"
-        }
-        ta.recycle()
+    override fun persistFloat(value: Float): Boolean {
+        return super.persistInt(value.roundToInt())
     }
 
-    override fun onBindViewHolder(holder: PreferenceViewHolder) {
-        super.onBindViewHolder(holder)
-        val view = holder.itemView
-        mSeekbar = view.findViewById(R.id.seekbar)
-        mValueText = view.findViewById(R.id.txtValue)
-        mSeekbar!!.max = max
-        mSeekbar!!.min = min
-
-        current = getPersistedInt(defaultValue)
-        updateDisplayedValue()
-
-        if (allowResetToDefault) view.setOnCreateContextMenuListener(this)
-        ColorEngine.getInstance(context).addColorChangeListeners(this, ColorEngine.Resolvers.ACCENT)
-    }
-
-    override fun onColorChange(resolveInfo: ColorEngine.ResolveInfo) {
-        if (resolveInfo.key == ColorEngine.Resolvers.ACCENT) {
-            val stateList = ColorStateList.valueOf(resolveInfo.color)
-            mSeekbar?.apply {
-                thumbTintList = stateList
-                progressTintList = stateList
-                progressBackgroundTintList = stateList
-            }
-        }
-    }
-
-    override fun onDetached() {
-        super.onDetached()
-        ColorEngine.getInstance(context).removeColorChangeListeners(this, ColorEngine.Resolvers.ACCENT)
-    }
-
-    fun setValue(value: Int) {
-        current = value
-        persistInt(value)
-        updateDisplayedValue()
-    }
-
-    protected open fun updateDisplayedValue() {
-        mSeekbar?.setOnSeekBarChangeListener(null)
-        val progress = current
-        mSeekbar!!.progress = progress
-        updateSummary()
-        mSeekbar?.setOnSeekBarChangeListener(this)
-    }
-
-    override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
-        current = progress
-        updateSummary()
-    }
-
-    protected open fun updateSummary() {
-        mValueText!!.text = String.format(format!!, current * multiplier)
-    }
-
-    override fun onStartTrackingTouch(seekBar: SeekBar) {}
-
-    override fun onStopTrackingTouch(seekBar: SeekBar) {
-        persistInt(current)
-    }
-
-    override fun persistInt(value: Int): Boolean {
-        if (value == lastPersist) return true
-        lastPersist = value
-        return super.persistInt(value)
-    }
-
-    override fun onCreateContextMenu(menu: ContextMenu, v: View, menuInfo: ContextMenu.ContextMenuInfo?) {
-        menu.setHeaderTitle(title)
-        menu.add(0, 0, 0, R.string.reset_to_default)
-        for (i in (0 until menu.size())) {
-            menu.getItem(i).setOnMenuItemClickListener(this)
-        }
-    }
-
-    override fun onMenuItemClick(item: MenuItem): Boolean {
-        setValue(defaultValue)
-        return true
+    override fun getPersistedFloat(defaultReturnValue: Float): Float {
+        return super.getPersistedInt(defaultReturnValue.roundToInt()).toFloat()
     }
 }
