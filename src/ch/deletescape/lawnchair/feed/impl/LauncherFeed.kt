e4e683fc9b6097c9ab9bb59de3979ba3c443e287
oldosfan <luangruo@yahoo.com>
2019-10-04 17:00:15 +0800

    feed: fix animation switching to an empty tab
    Signed-off-by: oldosfan <luangruo@yahoo.com>

--- src/ch/deletescape/lawnchair/feed/impl/LauncherFeed.kt
+++ src/ch/deletescape/lawnchair/feed/impl/LauncherFeed.kt
@@ -1247,7 +1247,7 @@ class LauncherFeed(val originalContext: Context,
                         }
                     } else {
                         recyclerView.isLayoutFrozen = false
-                        adapter.notifyItemRangeRemoved(0, oldCards.size)
+                        adapter.notifyItemRangeRemoved(0, oldCards.size - 1)
                     }
                     recyclerView.isLayoutFrozen = false
                     recyclerView.visibility = View.VISIBLE
