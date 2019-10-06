b0642f0cf8560c78600a6d7769162eedde122f86
oldosfan <luangruo@yahoo.com>
2019-10-06 11:11:41 +0800

    feed: "fix" suggested apps
    Signed-off-by: oldosfan <luangruo@yahoo.com>

--- lawnchair/src/ch/deletescape/lawnchair/predictions/PredictedApplicationsAdapter.kt
+++ lawnchair/src/ch/deletescape/lawnchair/predictions/PredictedApplicationsAdapter.kt
@@ -21,13 +21,11 @@ package ch.deletescape.lawnchair.predictions
 
 import android.content.Context
 import android.content.Intent
-import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import ch.deletescape.lawnchair.allapps.ParcelableComponentKeyMapper
 import ch.deletescape.lawnchair.atMost
-import com.android.launcher3.BubbleTextView
-import com.android.launcher3.R
 
 open class PredictedApplicationsAdapter(val context: Context) :
         RecyclerView.Adapter<IconViewViewHolder>() {
@@ -44,16 +42,14 @@ open class PredictedApplicationsAdapter(val context: Context) :
         val title = context.packageManager.getActivityInfo(
                 mapper.componentKey.componentName, 0).loadLabel(
                 context.packageManager).toString()
-        holder.bubbleTextView.text = title
-        holder.bubbleTextView.mIcon = icon
-        holder.bubbleTextView.setOnClickListener {
+        holder.view.setImageDrawable(icon)
+        holder.view.setOnClickListener {
             context.startActivity(Intent().setComponent(
                     mapper.componentKey.componentName).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK))
         }
     }
 }
 
-class IconViewViewHolder(parent: ViewGroup) : androidx.recyclerview.widget.RecyclerView.ViewHolder(
-        LayoutInflater.from(parent.context).inflate(R.layout.all_apps_icon, parent, false)) {
-    val bubbleTextView by lazy { itemView as BubbleTextView }
+class IconViewViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(ImageView(parent.context)) {
+    internal val view = super.itemView as ImageView
 }
