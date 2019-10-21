a526a5ae45b571713fffc3aa45a4bfd5ba3491c6
oldosfan <luangruo@yahoo.com>
2019-10-21 11:30:17 +0800

    chips: fix rounded shape corner-radius
    Signed-off-by: oldosfan <luangruo@yahoo.com>

--- lawnchair/src/ch/deletescape/lawnchair/feed/chips/ChipStyleRegistry.java
+++ lawnchair/src/ch/deletescape/lawnchair/feed/chips/ChipStyleRegistry.java
@@ -24,6 +24,7 @@ import android.content.Context;
 
 import com.android.launcher3.R;
 import com.google.android.material.shape.CutCornerTreatment;
+import com.google.android.material.shape.RoundedCornerTreatment;
 import com.google.android.material.shape.ShapeAppearanceModel;
 
 import java.util.LinkedHashMap;
@@ -35,13 +36,15 @@ public final class ChipStyleRegistry {
     public static final Map<String, Integer> NAMES = new LinkedHashMap<>();
     public static final String ROUND = "rnd";
     public static final String CUT = "cut";
+
     static {
         NAMES.put(ROUND, R.string.title_chip_style_round);
         NAMES.put(CUT, R.string.title_chip_style_cut);
     }
 
     public static void populateWithContext(Context c) {
-        ALL.put(ROUND, new ShapeAppearanceModel());
+        ALL.put(ROUND, new ShapeAppearanceModel.Builder().setAllCorners(
+                new RoundedCornerTreatment()).setAllCornerSizes(64f).build());
         ShapeAppearanceModel cut = new ShapeAppearanceModel.Builder().setAllCornerSizes(
                 8f).setAllCorners(new CutCornerTreatment()).build();
         ALL.put(CUT, cut);
