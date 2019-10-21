ea8d8cc8d82dae6a78abba281133e54ac245d0cd
oldosfan <luangruo@yahoo.com>
2019-10-21 13:29:45 +0800

    feed: use spring effect in the weather carousel
    Signed-off-by: oldosfan <luangruo@yahoo.com>

---
+++ lawnchair/src/ch/deletescape/lawnchair/views/SpringHorizontalScrollView.kt
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2019 oldosfan.
+ * Copyright (c) 2019 the Lawnchair developers
+ *
+ *     This file is part of Librechair.
+ *
+ *     Librechair is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Librechair is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Librechair.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+/*
+ *     This file is part of Lawnchair Launcher.
+ *
+ *     Lawnchair Launcher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Lawnchair Launcher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Lawnchair Launcher.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package ch.deletescape.lawnchair.views
+
+import android.content.Context
+import android.graphics.Canvas
+import android.util.AttributeSet
+import android.view.View
+import android.widget.HorizontalScrollView
+import androidx.recyclerview.widget.RecyclerView.EdgeEffectFactory.DIRECTION_RIGHT
+import ch.deletescape.lawnchair.util.getField
+import com.android.launcher3.Utilities
+
+open class SpringHorizontalScrollView @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : HorizontalScrollView(context, attrs, defStyleAttr) {
+
+    private val springManager = SpringEdgeEffect.Manager(this)
+
+    open var shouldTranslateSelf = true
+
+    var isTopFadingEdgeEnabled = true
+
+    init {
+        if (Utilities.HIDDEN_APIS_ALLOWED) {
+            getField<HorizontalScrollView>("mEdgeGlowLeft").set(this, springManager.createEdgeEffect(
+                    DIRECTION_RIGHT, true))
+            getField<HorizontalScrollView>("mEdgeGlowRight").set(this, springManager.createEdgeEffect(
+                    DIRECTION_RIGHT, true))
+        }
+        overScrollMode = View.OVER_SCROLL_ALWAYS
+    }
+
+    override fun draw(canvas: Canvas) {
+        springManager.withSpring(canvas, shouldTranslateSelf) {
+            super.draw(canvas)
+            false
+        }
+    }
+
+    override fun dispatchDraw(canvas: Canvas) {
+        springManager.withSpring(canvas, !shouldTranslateSelf) {
+            super.dispatchDraw(canvas)
+            false
+        }
+    }
+
+    override fun getTopFadingEdgeStrength(): Float {
+        return if (isTopFadingEdgeEnabled) super.getTopFadingEdgeStrength() else 0f
+    }
+}
