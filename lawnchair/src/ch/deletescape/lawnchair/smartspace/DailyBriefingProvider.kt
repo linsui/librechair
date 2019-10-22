6bb3cda555f61d0db5b07d6287ee7e4ca6544a19
oldosfan <luangruo@yahoo.com>
2019-10-22 11:57:22 +0800

    smartspace: retrofit daily briefing provider to use the new WeatherManager
    Signed-off-by: oldosfan <luangruo@yahoo.com>

--- lawnchair/src/ch/deletescape/lawnchair/smartspace/DailyBriefingProvider.kt
+++ lawnchair/src/ch/deletescape/lawnchair/smartspace/DailyBriefingProvider.kt
@@ -23,6 +23,7 @@ import android.database.Cursor
 import android.provider.CalendarContract
 import android.text.TextUtils
 import ch.deletescape.lawnchair.*
+import ch.deletescape.lawnchair.awareness.WeatherManager
 import ch.deletescape.lawnchair.smartspace.weather.forecast.ForecastProvider
 import ch.deletescape.lawnchair.util.extensions.d
 import com.android.launcher3.R
@@ -32,7 +33,15 @@ import java.util.concurrent.TimeUnit
 class DailyBriefingProvider(controller: LawnchairSmartspaceController) :
         LawnchairSmartspaceController.PeriodicDataProvider(controller) {
     override val timeout = TimeUnit.SECONDS.toMillis(10)
-    var briefingShown = false to 0L
+    private var briefingShown = false to 0L
+    private lateinit var forecast: ForecastProvider.Forecast
+    private lateinit var currentWeather: ForecastProvider.CurrentWeather
+
+    init {
+        WeatherManager.subscribeHourly { forecast = it }
+        WeatherManager.subscribeWeather { currentWeather = it }
+    }
+
     override fun updateData() {
         var currentTime = GregorianCalendar()
         val (hour, minute) = context.lawnchairPrefs.wakeUpCallTime.split(":").map { it.toInt() }
@@ -40,105 +49,106 @@ class DailyBriefingProvider(controller: LawnchairSmartspaceController) :
         if (currentTime.hourOfDay == hour && currentTime.minuteOfHour >= minute - 10 && currentTime.minuteOfHour <= minute + 10) {
             d("updateData: updating daily brief")
             runOnUiWorkerThread {
-                if (!briefingShown.first || briefingShown.second < System.currentTimeMillis()) {
-                    try {
-                        val (lat, lon) = context.forecastProvider.getGeolocation(
-                                context.lawnchairPrefs.weatherCity)
-                        d("updateData: retrieved location: $lat, $lon")
-                        val currentWeather = context.forecastProvider.getCurrentWeather(lat, lon)
-                        d("updateData: retrieved current weather $currentWeather")
-                        val hourlyForecast = context.forecastProvider.getHourlyForecast(lat, lon)
-                        d("updateData: retrieved forecasts $currentWeather and $hourlyForecast")
-                        val lines = mutableListOf(LawnchairSmartspaceController.Line(
-                                context.getString(
-                                        R.string.daily_briefing_current_conditions_prefix) + " " + currentWeather.temperature.toString(
-                                        context.lawnchairPrefs.weatherUnit),
-                                TextUtils.TruncateAt.MARQUEE), LawnchairSmartspaceController.Line(
-                                context.getString(
-                                        R.string.daily_breifing_today_prefix) + " " + run {
-                                    val (clear, clouds, rain, snow, thunder) = WeatherTypes.getStatistics(
-                                            run {
-                                                val list = newList<Int>()
-                                                hourlyForecast.data.filter {
-                                                    it.date.before(tomorrow())
-                                                }.forEach {
-                                                    list += it.condCode?.toList() ?: listOf(1)
-                                                }
-                                                d("updateData: parsed condition codes $list")
-                                                list.toTypedArray()
-                                            })
-                                    d("updateData: parsed condition type ${WeatherTypes.getStringResource(
-                                            WeatherTypes.getWeatherTypeFromStatistics(clear, clouds,
-                                                                                      rain, snow,
-                                                                                      thunder)).fromStringRes(
-                                            context).toLowerCase()}")
-                                    WeatherTypes.getStringResource(
-                                            WeatherTypes.getWeatherTypeFromStatistics(clear, clouds,
-                                                                                      rain, snow,
-                                                                                      thunder))
-                                            .fromStringRes(context).toLowerCase()
-                                }, TextUtils.TruncateAt.MARQUEE))
-                        val query =
-                                "(( " + CalendarContract.Events.DTSTART + " >= " + currentTime.getTimeInMillis() + " ) AND ( " + CalendarContract.Events.DTSTART + " <= " + tomorrow().time + " ))"
-                        var hasCalendar = false
+                if (::forecast.isInitialized && ::currentWeather.isInitialized) {
+                    if (!briefingShown.first || briefingShown.second < System.currentTimeMillis()) {
                         try {
-                            val eventCursor: Cursor =
-                                    context.contentResolver.query(
-                                            CalendarContract.Events.CONTENT_URI,
-                                            arrayOf(CalendarContract.Instances.TITLE,
-                                                    CalendarContract.Instances.DTSTART,
-                                                    CalendarContract.Instances.DTEND,
-                                                    CalendarContract.Instances.DESCRIPTION,
-                                                    CalendarContract.Events._ID,
-                                                    CalendarContract.Instances.CUSTOM_APP_PACKAGE,
-                                                    CalendarContract.Events.EVENT_LOCATION),
-                                            query, null,
-                                            CalendarContract.Instances.DTSTART + " ASC")!!
+                            val lines = mutableListOf(LawnchairSmartspaceController.Line(
+                                    context.getString(
+                                            R.string.daily_briefing_current_conditions_prefix) + " " + currentWeather.temperature.toString(
+                                            context.lawnchairPrefs.weatherUnit),
+                                    TextUtils.TruncateAt.MARQUEE),
+                                    LawnchairSmartspaceController.Line(
+                                            context.getString(
+                                                    R.string.daily_breifing_today_prefix) + " " + run {
+                                                val (clear, clouds, rain, snow, thunder) = WeatherTypes.getStatistics(
+                                                        run {
+                                                            val list = newList<Int>()
+                                                            forecast.data.filter {
+                                                                it.date.before(tomorrow())
+                                                            }.forEach {
+                                                                list += it.condCode?.toList()
+                                                                        ?: listOf(1)
+                                                            }
+                                                            d("updateData: parsed condition codes $list")
+                                                            list.toTypedArray()
+                                                        })
+                                                d("updateData: parsed condition type ${WeatherTypes.getStringResource(
+                                                        WeatherTypes.getWeatherTypeFromStatistics(
+                                                                clear, clouds,
+                                                                rain, snow,
+                                                                thunder)).fromStringRes(
+                                                        context).toLowerCase()}")
+                                                WeatherTypes.getStringResource(
+                                                        WeatherTypes.getWeatherTypeFromStatistics(
+                                                                clear, clouds,
+                                                                rain, snow,
+                                                                thunder))
+                                                        .fromStringRes(context).toLowerCase()
+                                            }, TextUtils.TruncateAt.MARQUEE))
+                            val query =
+                                    "(( " + CalendarContract.Events.DTSTART + " >= " + currentTime.getTimeInMillis() + " ) AND ( " + CalendarContract.Events.DTSTART + " <= " + tomorrow().time + " ))"
+                            var hasCalendar = false
+                            try {
+                                val eventCursor: Cursor =
+                                        context.contentResolver.query(
+                                                CalendarContract.Events.CONTENT_URI,
+                                                arrayOf(CalendarContract.Instances.TITLE,
+                                                        CalendarContract.Instances.DTSTART,
+                                                        CalendarContract.Instances.DTEND,
+                                                        CalendarContract.Instances.DESCRIPTION,
+                                                        CalendarContract.Events._ID,
+                                                        CalendarContract.Instances.CUSTOM_APP_PACKAGE,
+                                                        CalendarContract.Events.EVENT_LOCATION),
+                                                query, null,
+                                                CalendarContract.Instances.DTSTART + " ASC")!!
 
-                            if (eventCursor.count >= 1) {
-                                hasCalendar = true
-                                lines += LawnchairSmartspaceController
-                                        .Line(R.plurals.title_daily_briefing_calendar_events.fromPluralRes(
-                                                context, eventCursor.count))
+                                if (eventCursor.count >= 1) {
+                                    hasCalendar = true
+                                    lines += LawnchairSmartspaceController
+                                            .Line(R.plurals.title_daily_briefing_calendar_events.fromPluralRes(
+                                                    context, eventCursor.count))
+                                }
+                                eventCursor.close()
+                            } catch (e: SecurityException) {
+                                e.printStackTrace()
                             }
-                            eventCursor.close()
-                        } catch (e: SecurityException) {
-                            e.printStackTrace()
-                        }
 
-                        d("updateData: formatted information into $lines")
+                            d("updateData: formatted information into $lines")
 
-                        runOnMainThread {
-                            var updateDataWithEvents: () -> Unit = {}
-                            val updateDataNoEvents: () -> Unit = {
-                                currentTime = GregorianCalendar()
-                                if (currentTime.hourOfDay == hour && currentTime.minuteOfHour >= minute - 10 && currentTime.minuteOfHour <= minute + 10) {
-                                    updateData(null,
-                                               LawnchairSmartspaceController.CardData(null, lines,
-                                                                                      forceSingleLine = false))
-                                    if (hasCalendar) {
-                                        mainHandler.postDelayed(updateDataWithEvents, 1000 * 2)
+                            runOnMainThread {
+                                var updateDataWithEvents: () -> Unit = {}
+                                val updateDataNoEvents: () -> Unit = {
+                                    currentTime = GregorianCalendar()
+                                    if (currentTime.hourOfDay == hour && currentTime.minuteOfHour >= minute - 10 && currentTime.minuteOfHour <= minute + 10) {
+                                        updateData(null,
+                                                LawnchairSmartspaceController.CardData(null, lines,
+                                                        forceSingleLine = false))
+                                        if (hasCalendar) {
+                                            mainHandler.postDelayed(updateDataWithEvents, 1000 * 2)
+                                        }
+                                    } else {
+                                        updateData(null, null)
                                     }
-                                } else {
-                                    updateData(null, null)
                                 }
-                            }
-                            updateDataWithEvents = {
-                                if (currentTime.hourOfDay == hour && currentTime.minuteOfHour >= minute - 10 && currentTime.minuteOfHour <= minute + 10) {
-                                    updateData(null, LawnchairSmartspaceController.CardData(null, listOf(lines[1]),
-                                                                                            forceSingleLine = false))
-                                    mainHandler.postDelayed(updateDataNoEvents, 1000 * 2)
-                                } else {
-                                    updateData(null, null)
+                                updateDataWithEvents = {
+                                    if (currentTime.hourOfDay == hour && currentTime.minuteOfHour >= minute - 10 && currentTime.minuteOfHour <= minute + 10) {
+                                        updateData(null,
+                                                LawnchairSmartspaceController.CardData(null,
+                                                        listOf(lines[1]),
+                                                        forceSingleLine = false))
+                                        mainHandler.postDelayed(updateDataNoEvents, 1000 * 2)
+                                    } else {
+                                        updateData(null, null)
+                                    }
                                 }
+                                updateDataNoEvents()
+                                briefingShown = true to System.currentTimeMillis() + 1000 * 60 * 10
                             }
-                            updateDataNoEvents()
-                            briefingShown = true to System.currentTimeMillis() + 1000 * 60 * 10
+                        } catch (e: ForecastProvider.ForecastException) {
+                            e.printStackTrace()
+                        } catch (e: NullPointerException) {
+                            e.printStackTrace();
                         }
-                    } catch (e: ForecastProvider.ForecastException) {
-                        e.printStackTrace()
-                    } catch (e: NullPointerException) {
-                        e.printStackTrace();
                     }
                 }
             }
