8135340440d2c3212adb846e3929f326330206b1
oldosfan <luangruo@yahoo.com>
2019-11-24 12:48:35 +0800

    feed: display alarm info when clicked in the alarm event provider

--- lawnchair/src/ch/deletescape/lawnchair/feed/AlarmEventProvider.java
+++ lawnchair/src/ch/deletescape/lawnchair/feed/AlarmEventProvider.java
@@ -21,6 +21,7 @@ package ch.deletescape.lawnchair.feed;
 
 import android.app.AlarmManager;
 import android.app.AlarmManager.AlarmClockInfo;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.view.View;
@@ -33,6 +34,8 @@ import java.util.List;
 import java.util.Objects;
 
 import ch.deletescape.lawnchair.LawnchairUtilsKt;
+import ch.deletescape.lawnchair.feed.util.FeedUtil;
+import kotlin.Unit;
 
 public class AlarmEventProvider extends FeedProvider {
 
@@ -61,11 +64,18 @@ public class AlarmEventProvider extends FeedProvider {
             Drawable alarm = getContext().getDrawable(R.drawable.ic_alarm_on_black_24dp);
             alarm = LawnchairUtilsKt.tint(Objects.requireNonNull(alarm),
                     FeedAdapter.Companion.getOverrideColor(getContext()));
-            return Collections.singletonList(new Card(alarm,
+            return Collections.singletonList(FeedUtil.apply(new Card(alarm,
                     LawnchairUtilsKt.formatTime(new Date(info.getTriggerTime()), getContext()),
                     parent -> new View(getContext()),
                     Card.RAISE | Card.TEXT_ONLY,
-                    "nosort,top", "alarmEvent".hashCode()));
+                    "nosort,top", "alarmEvent".hashCode()), it -> it.globalClickListener = view -> {
+                try {
+                    info.getShowIntent().send();
+                } catch (PendingIntent.CanceledException e) {
+                    e.printStackTrace();
+                }
+                return Unit.INSTANCE;
+            }));
         }
         return Collections.emptyList();
     }
