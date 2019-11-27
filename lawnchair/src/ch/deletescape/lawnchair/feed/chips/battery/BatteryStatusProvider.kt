b072e770542afbe555f8df6f04f2d38e234fcd82
oldosfan <luangruo@yahoo.com>
2019-11-27 16:47:23 +0800

    chips: don't rely on controller refreshes to refresh battery chip

--- lawnchair/src/ch/deletescape/lawnchair/feed/chips/battery/BatteryStatusProvider.kt
+++ lawnchair/src/ch/deletescape/lawnchair/feed/chips/battery/BatteryStatusProvider.kt
@@ -26,13 +26,17 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.os.BatteryManager
 import ch.deletescape.lawnchair.feed.FeedAdapter
+import ch.deletescape.lawnchair.feed.chips.ChipItemBridge
 import ch.deletescape.lawnchair.feed.chips.ChipProvider
 import ch.deletescape.lawnchair.feed.util.FeedUtil
 import ch.deletescape.lawnchair.fromStringRes
 import com.android.launcher3.R
+import java.util.*
 import java.util.function.Consumer
 
 class BatteryStatusProvider(val context: Context) : ChipProvider() {
+
+    private val changeListeners = Vector<() -> Unit>()
 
     private fun updateData(intent: Intent?) {
         if (intent != null) {
@@ -48,6 +52,7 @@ class BatteryStatusProvider(val context: Context) : ChipProvider() {
                         * intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
                         / intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100)).toInt()
             }
+            changeListeners.forEach { it() }
         }
     }
 
@@ -64,8 +69,8 @@ class BatteryStatusProvider(val context: Context) : ChipProvider() {
                 level = (100f
                         * intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
                         / intent.getIntExtra(BatteryManager.EXTRA_SCALE, 100)).toInt()
-                refresh()
             }
+            changeListeners.forEach { it() }
         }
     }
 
@@ -77,7 +82,7 @@ class BatteryStatusProvider(val context: Context) : ChipProvider() {
         context.registerReceiver(reciever, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
     }
 
-    override fun getItems(context: Context): List<ChipProvider.Item> {
+    override fun getItems(context: Context): List<Item> {
         updateData(context.registerReceiver(null, IntentFilter(Intent.ACTION_BATTERY_CHANGED)))
         val lines = mutableListOf<Any>()
         when {
@@ -88,7 +93,31 @@ class BatteryStatusProvider(val context: Context) : ChipProvider() {
         if (!full) {
             lines.add("$level%")
         }
-        return listOf(ChipProvider.Item().apply {
+        return listOf(object : Item(){
+            override fun bindVoodo(bridge: ChipItemBridge) {
+                changeListeners += {
+                    val lines1 = mutableListOf<Any>()
+                    when {
+                        full -> lines1.add(R.string.battery_full)
+                        charging -> lines1.add(R.string.battery_charging)
+                        level <= 15 -> lines1.add(R.string.battery_low)
+                    }
+                    if (!full) {
+                        lines1.add("$level%")
+                    }
+                    bridge.setTitle(lines1.joinToString(" - ") {
+                        if (it is Int) it.fromStringRes(context) else it as CharSequence
+                    })
+                    bridge.setIcon(BatteryMeterDrawableBase(context, FeedAdapter.getOverrideColor(context)).apply {
+                        this.batteryLevel = this@BatteryStatusProvider.level
+                        this.charging = this@BatteryStatusProvider.charging
+                    })
+                    bridge.setOnClickListener {
+                        FeedUtil.startActivity(context, Intent(Intent.ACTION_POWER_USAGE_SUMMARY), it)
+                    }
+                }
+            }
+        }.apply {
             title = lines.joinToString(" - ") {
                 if (it is Int) it.fromStringRes(context) else it as CharSequence
             }
