c45d36f9c2aeff4a2b71681cc9fff351aa9ab396
oldosfan <luangruo@yahoo.com>
2019-11-28 13:08:18 +0800

    feed: get rid of the unused CacheManager

--- lawnchair/src/ch/deletescape/lawnchair/feed/cache/CacheDatabase.java
+++
@@ -1,9 +0,0 @@
-package ch.deletescape.lawnchair.feed.cache;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {CacheEntry.class}, version = 1)
-public abstract class CacheDatabase extends RoomDatabase {
-    abstract CacheDao access();
-}
