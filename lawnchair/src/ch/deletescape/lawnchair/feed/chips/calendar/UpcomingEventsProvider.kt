de72a16c79972144fe906eaa4748fa4649d8ea6b
oldosfan <luangruo@yahoo.com>
2019-12-02 13:27:53 +0800

    chips: port UEP to the CM

--- lawnchair/src/ch/deletescape/lawnchair/feed/chips/calendar/UpcomingEventsProvider.kt
+++ lawnchair/src/ch/deletescape/lawnchair/feed/chips/calendar/UpcomingEventsProvider.kt
@@ -23,128 +23,78 @@ package ch.deletescape.lawnchair.feed.chips.calendar
 import android.annotation.SuppressLint
 import android.content.ActivityNotFoundException
 import android.content.Context
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.database.Cursor
-import android.net.Uri
-import android.provider.CalendarContract
+import ch.deletescape.lawnchair.awareness.CalendarManager
+import ch.deletescape.lawnchair.awareness.TickManager
+import ch.deletescape.lawnchair.feed.CalendarScope
 import ch.deletescape.lawnchair.feed.FeedScope
 import ch.deletescape.lawnchair.feed.chips.ChipProvider
 import ch.deletescape.lawnchair.feed.maps.MapScreen
 import ch.deletescape.lawnchair.feed.maps.locationsearch.LocationSearchManager
 import ch.deletescape.lawnchair.feed.util.FeedUtil
 import ch.deletescape.lawnchair.fromDrawableRes
-import ch.deletescape.lawnchair.getPositionOnScreen
 import ch.deletescape.lawnchair.lawnchairPrefs
 import com.android.launcher3.R
-import com.google.android.apps.nexuslauncher.graphics.IcuDateTextView
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import java.time.Instant
-import java.util.*
+import okhttp3.internal.toImmutableList
+import java.time.LocalDateTime
 import java.util.function.Consumer
 
 class UpcomingEventsProvider(val context: Context) : ChipProvider() {
+    private val events = mutableListOf<CalendarManager.CalendarEvent>()
+
+    init {
+        CalendarManager.subscribe { ce ->
+            events.toImmutableList()
+            events.clear()
+            events += ce.filter {
+                it.startTime >= LocalDateTime.now() &&
+                        it.startTime <= LocalDateTime.now()
+                        .plusDays(context.lawnchairPrefs.feedCalendarEventThreshold.toLong())
+            }
+        }
+
+        TickManager.subscribe {
+            CalendarScope.launch {
+                val backup = listOf(* events.toTypedArray())
+                events.clear()
+                events += backup.filter {
+                    it.startTime >= LocalDateTime.now() &&
+                            it.startTime <= LocalDateTime.now()
+                            .plusDays(context.lawnchairPrefs.feedCalendarEventThreshold.toLong())
+                }
+            }
+        }
+    }
+
     @SuppressLint("Recycle")
     override fun getItems(context: Context):
-            List<Item> {
-        val chips = mutableListOf<Item>()
-        val currentTime = GregorianCalendar()
-        val endTime = GregorianCalendar().apply {
-            add(Calendar.DAY_OF_MONTH, context.lawnchairPrefs.feedCalendarEventThreshold)
-        }
-        val query =
-                "(( " + CalendarContract.Events.DTSTART + " >= " + currentTime.timeInMillis + " ) AND ( " + CalendarContract.Events.DTSTART + " <= " + endTime.timeInMillis + " ))"
-        try {
-            val eventCursor: Cursor = context.contentResolver
-                    .query(CalendarContract.Events.CONTENT_URI,
-                            arrayOf(CalendarContract.Instances.TITLE,
-                                    CalendarContract.Instances.DTSTART,
-                                    CalendarContract.Instances.DTEND,
-                                    CalendarContract.Instances.DESCRIPTION,
-                                    CalendarContract.Events._ID,
-                                    CalendarContract.Instances.CUSTOM_APP_PACKAGE,
-                                    CalendarContract.Events.EVENT_LOCATION,
-                                    CalendarContract.Calendars.CALENDAR_COLOR), query, null,
-                            CalendarContract.Instances.DTSTART + " ASC")!!
-            eventCursor.moveToFirst()
-            while (eventCursor.isAfterLast.not()) {
-                val startTime = GregorianCalendar()
-                startTime.timeInMillis = eventCursor.getLong(1)
-                val eventEndTime = GregorianCalendar()
-                eventEndTime.timeInMillis = eventCursor.getLong(2)
-                val text: String
-                val diff = startTime.timeInMillis - currentTime.timeInMillis
-                val diffMinutes = diff / (60 * 1000)
-                val diffHours = diff / (60 * 60 * 1000)
-                val diffDays = diff / (24 * 60 * 60 * 1000)
-                when {
-                    diffDays > 20 -> text = IcuDateTextView.getDateFormat(context, true, null, false)
-                            .format(Date.from(Instant.ofEpochMilli(startTime.timeInMillis)))
-                    diffDays >= 1 -> text = if (diffDays < 1 || diffDays > 1) context.getString(
-                            R.string.title_text_calendar_feed_provider_in_d_days,
-                            diffDays) else context.getString(R.string.tomorrow)
-                    diffHours > 4 -> text = context
-                            .getString(R.string.title_text_calendar_feed_in_d_hours, diffHours)
-                    else -> text = if (diffMinutes <= 0) context.getString(
-                            R.string.reusable_str_now) else context.getString(
-                            if (diffMinutes < 1 || diffMinutes > 1) R.string.subtitle_smartspace_in_minutes else R.string.subtitle_smartspace_in_minute,
-                            diffMinutes)
-                }
-                var intent = Intent(Intent.ACTION_VIEW)
-                intent.data = Uri
-                        .parse("content://com.android.calendar/events/" + eventCursor.getLong(
-                                4).toString())
-                if (eventCursor.getString(5) != null) {
-                    if (context.packageManager.getApplicationEnabledSetting(
-                                    eventCursor.getString(
-                                            5)!!) != PackageManager.COMPONENT_ENABLED_STATE_DISABLED) {
-                        intent.`package` = eventCursor.getString(5)!!
-                    }
-                }
-                val address = eventCursor.getString(6)
-                intent.flags = intent.flags or Intent.FLAG_ACTIVITY_NEW_TASK
-                if (address?.trim()?.isEmpty() == false) {
-                    intent = Intent(Intent.ACTION_VIEW, Uri.parse("geo:0,0?q=$address"))
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                }
-                chips += Item().apply {
-                    title = "${eventCursor.getString(0)} ($text)"
-                    icon = R.drawable.ic_event_black_24dp.fromDrawableRes(context)
-                    viewClickListener = Consumer {
-                        if (address?.trim()?.isEmpty() == false) {
-                            FeedScope.launch {
-                                val loc = LocationSearchManager.getInstance(context).get(address)
-                                if (loc != null) {
-                                    FeedScope.launch(Dispatchers.Main) {
-                                        MapScreen(context, launcherFeed, loc.first, loc.second,
-                                                13.0).display(launcherFeed,
-                                                it.getPositionOnScreen().first + it.measuredWidth / 2,
-                                                it.getPositionOnScreen().second + it.measuredHeight / 2, it)
-                                    }
-                                } else {
-                                    FeedScope.launch(Dispatchers.Main) {
-                                        try {
-                                            FeedUtil.startActivity(context, intent, it)
-                                        } catch (e: ActivityNotFoundException) {
-                                            e.printStackTrace()
-                                        }
-                                    }
+            List<Item> = events.map {
+        Item().apply {
+            title = it.title
+            icon = R.drawable.ic_event_black_24dp.fromDrawableRes(context)
+            viewClickListener = Consumer { v ->
+                if (it.address != null) {
+                    FeedScope.launch {
+                        val loc = LocationSearchManager.getInstance(context).get(it.address)
+                        if (loc != null) {
+                            FeedScope.launch(Dispatchers.Main) {
+                                MapScreen(context, launcherFeed, loc.first, loc.second,
+                                        13.0).display(launcherFeed, null, null, v)
+                            }
+                        } else {
+                            FeedScope.launch(Dispatchers.Main) {
+                                try {
+                                    FeedUtil.startActivity(context, it.intent, v)
+                                } catch (e: ActivityNotFoundException) {
+                                    e.printStackTrace()
                                 }
                             }
                         }
                     }
                 }
-                eventCursor.moveToNext()
             }
-            eventCursor.close()
-        } catch (e: SecurityException) {
-            e.printStackTrace()
-            return listOf(Item().apply {
-                title = context.getString(R.string.title_chip_need_calendar_permissions)
-            })
         }
-        return chips
     }
 
 }
