8f70d9f5e7c4d77c512983c006333ad137fd402d
oldosfan <luangruo@yahoo.com>
2019-12-19 17:59:14 +0800

    feed: raise max redir depth to 10

--- lawnchair/src/ch/deletescape/lawnchair/feed/util/NetworkUtil.java
+++ lawnchair/src/ch/deletescape/lawnchair/feed/util/NetworkUtil.java
@@ -49,7 +49,7 @@ public final class NetworkUtil {
         if (System.currentTimeMillis() - startTime > TimeUnit.SECONDS.toMillis(25)) {
             return url;
         }
-        if (count > 6) {
+        if (count > 10) {
             return url;
         } else {
             URL urlV = new URL(url);
